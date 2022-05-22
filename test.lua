f41816c1d0a46eda482fdf6d73b8179bc660fa22
Hezaus <ongmeo3316@gmail.com>
2022-05-22 11:25:09 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -28,4 +28,3 @@ local HttpService = game:GetService("HttpService")
 
 local a = HttpService:GetAsync("https://raw.githubusercontent.com/Hezaus/Testing/main/requrie.lua")
 local b = HttpService:JSONDecode(a)
-print(b)
